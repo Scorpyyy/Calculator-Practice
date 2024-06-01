03f0a6e6fb51a3fe09380e238acf7fadfb59395e
Scorpyyy <scorpyboyz793@gmail.com>
2024-06-01 13:39:31 +0800

    I added "help" command

--- Calculator Practice/Calculator Practice.cpp
+++ Calculator Practice/Calculator Practice.cpp
@@ -1,17 +1,95 @@
 // Calculator Practice.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "Calculator.h"
 #include <iostream>
+#include <sstream>
+#include <limits>
 
 using namespace std;
 
+
+
+
+bool isValidNumber(const string& str) {
+	std::istringstream iss(str);
+	double num;
+	iss >> num;
+	return iss.eof() && !iss.fail();
+}
+
+
+
 int main()
 {
+	double x = 0.0;
+	double y = 0.0;
+	double result = 0.0;
+	char oper = '+';
+
+
 	cout << "Calculator Console Application" << endl << endl;
 	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
 		<< endl;
+
+	Calculator c;
+	while (true)
+	{
+		string input;
+		cout << "Enter Your Expression: ";
+		cin >> input;
+
+		if (input == "help") {
+			cout << "Need Help? STFU" << endl;
+			continue;
+		}
+
+		size_t operPos = input.find_first_of("+-*/");
+
+
+		if (operPos == string::npos) {
+			cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+
+		}
+		string xStr = input.substr(0, operPos);
+		string yStr = input.substr(operPos + 1);
+		oper = input[operPos];
+
+
+
+
+
+		if (!isValidNumber(xStr) || !isValidNumber(yStr)) {
+			cout << "Pls Enter just numbers." << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+
+		}
+
+		x = stod(xStr);
+		y = stod(yStr);
+
+		if (oper == '/' && y == 0) {
+			cout << "Attempted to Devide by zero!" << endl;
+			continue;
+		}
+
+		else {
+			result = c.Calculate(x, oper, y);
+		}
+
+		cout << "Result " << "Of " << x << oper << y << " is: " << result << endl;
+	}
+
+
 	return 0;
 }
+
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
---
+++ Calculator Practice/letter-filter.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <cctype>
+#include <limits>
+
